29f2fd808d6e102c56698b931e9c25c898c86e86
Madhusudanan Kandasamy <MKandasamy1@statestreet.com>
2019-06-03 16:17:01 +0530

    fix

--- pkg1/pkg1_sub1/setup.py
+++ pkg1/pkg1_sub1/setup.py
@@ -1,14 +1,14 @@
 import setuptools,os
 print(f'running pk1.pkg1_sub1.setup @ {os.getcwd()}')
 setuptools.setup(
-    name="pkg-examples",
+    name="pkg_example",
     version="0.1",
     author="madhusudanan kandasamy",
     author_email="madhukandasamy@yahoo.com",
     description="A small example package",
     url="https://github.com/kmadhugit/pkg_example.git",
-    package_dir={'':'../'},
-    packages=['pkg1_sub1'],
+    package_dir={'':'../../../'},
+    packages=['pkg_example.pkg1.pkg1_sub1'],
     install_requires = [
         'flask'
         ]
