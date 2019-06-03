f097934d06ae207cedb41b8b10fce5c0128f8b18
Madhusudanan Kandasamy <MKandasamy1@statestreet.com>
2019-06-03 21:47:58 +0530

    fix

--- pkg1/pkg1_sub1/setup.py
+++ pkg1/pkg1_sub1/setup.py
@@ -1,5 +1,11 @@
-import setuptools,os
-print(f'running pk1.pkg1_sub1.setup @ {os.getcwd()}')
+import setuptools
+import os
+from os.path import dirname
+module_dir = dirname(__file__)
+root_dir = f'{module_dir}/../../../'
+os.chdir(root_dir)
+print(f'running pk1.pkg1_sub1.setup @ {os.getcwd()} root_dir={root_dir}')
+
 setuptools.setup(
     name="pkg_example",
     version="0.1",
@@ -7,8 +13,8 @@ setuptools.setup(
     author_email="madhukandasamy@yahoo.com",
     description="A small example package",
     url="https://github.com/kmadhugit/pkg_example.git",
-    package_dir={'':'../../'},
-    packages=['pkg1.pkg1_sub1'],
+    #package_dir={'':'../../'},
+    packages=['pkg_example.pkg1.pkg1_sub1'],
     install_requires = [
         'flask'
         ]
